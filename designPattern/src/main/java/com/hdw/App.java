5ff5cd6113861c8955ca1b4fd102ae95e5774d18
hudawei1 <hudawei1@jd.com>
2018-02-06 21:09:37 +0800

    设计模式模块配置log

--- designPattern/src/main/java/com/hdw/App.java
+++ designPattern/src/main/java/com/hdw/App.java
@@ -1,13 +1,17 @@
 package com.hdw;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        log.info( "Hello World!" );
     }
 }
