895bd78d1e43fac63a956e1caa59cf8edff50b55
hudawei1 <hudawei1@jd.com>
2018-02-06 21:08:01 +0800

    设计模式模块配置log

---
+++ designPattern/src/main/java/com/hdw/proxy/App.java
@@ -0,0 +1,18 @@
+package com.hdw.proxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * user:hudawei1
+ * date:2018/2/6
+ * time:20:16
+ */
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+
+    public static void main(String[] args){
+        log.info("========= start App ===========");
+    }
+}
