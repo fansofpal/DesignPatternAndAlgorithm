ee0a47e26549cdff60b5c3c782e87c62a244adac
hudawei1 <hudawei1@jd.com>
2018-04-17 21:19:06 +0800

    冒泡排序

---
+++ algorithm/src/main/java/com/hdw/sort/BaseArray.java
@@ -0,0 +1,12 @@
+package com.hdw.sort;
+
+/**
+ * user:hudawei1
+ * date:2018/4/12
+ * time:16:42
+ */
+public abstract class BaseArray {
+
+    protected static int[] randomArrays = new int[]{1,4,2,8,16,3,7,19,14,12,5,21,26,29,20,9,18};
+
+}
---
+++ algorithm/src/main/java/com/hdw/sort/BubbleSort.java
@@ -0,0 +1,45 @@
+package com.hdw.sort;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+/**
+ * user:hudawei1
+ * date:2018/4/17
+ * time:20:49
+ */
+public class BubbleSort extends BaseArray{
+    private static final Logger log = LoggerFactory.getLogger(BubbleSort.class);
+
+
+    public static void main(String[] args){
+        log.info("原数组："+ Arrays.toString(randomArrays));
+        bubbleSort(randomArrays);
+        log.info("新数组："+Arrays.toString(randomArrays));
+    }
+
+
+    /**
+     * 从小到大
+     * @param array
+     * @return
+     */
+    public static int[] bubbleSort(int[] array){
+        if(array == null || array.length == 0 || array.length == 1){
+            return array;
+        }
+        for(int i=0;i<array.length;i++){
+            for(int j=array.length-1;j>i;j--){
+                if(array[j-1]>array[j]){
+                    int temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+}
